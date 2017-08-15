2adeb51ecf2c944ff4647b809c7340089f09ca4a
Dave Brophy <dave@brophy.uk>
2017-08-15 23:30:11 +0100

    Comments

--- mockdb/database.go
+++ mockdb/database.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// MockDatabase
+// MockDatabase is a mock database to stand in for InfluxDB during development
 type MockDatabase struct {
 	MinResponseTime int
 	MaxResponseTime int
@@ -17,7 +17,7 @@ type MockDatabase struct {
 // Send sends the datapoint to the mock database, and returns via the finished channel
 func (m MockDatabase) Send(ctx context.Context, datapoint interface{}, finished chan error) {
 	go func() {
-		// Note use of rand.Intn without rand.Seed() in order to make
+		// Note use of rand.Intn without rand.Seed() ... better for testing?
 		ms := m.MinResponseTime + rand.Intn(m.MaxResponseTime)
 		<-time.After(time.Millisecond * time.Duration(ms))
 		if rand.Intn(100) < m.ErrorPercentage {
